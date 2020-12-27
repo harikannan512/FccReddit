93a84a8043141f113db1b4fc8a1554ea8d640f65
Kaizukamezi <harikannan512@gmail.com>
2020-12-27 07:44:08 +0530

    verification and authentication working

--- src/main/java/com/clone/fccreddit/model/User.java
+++ src/main/java/com/clone/fccreddit/model/User.java
@@ -6,10 +6,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
@@ -20,6 +17,7 @@ import java.time.Instant;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "users")
 public class User {
 
     @Id
