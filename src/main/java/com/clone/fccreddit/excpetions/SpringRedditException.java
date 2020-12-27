d91a2ed87c934b3a279da5d3b67cf78943dbc188
Kaizukamezi <harikannan512@gmail.com>
2020-12-27 05:34:28 +0530

    User Auth + User Verify 2

--- src/main/java/com/clone/fccreddit/excpetions/SpringRedditException.java
+++
@@ -1,7 +0,0 @@
-package com.clone.fccreddit.excpetions;
-
-public class SpringRedditException extends RuntimeException {
-    public SpringRedditException(String exMessage) {
-        super(exMessage);
-    }
-}
